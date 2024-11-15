a21ab05cbcd4852bee824747b2d241399e59f0b0
roachcd <161189623+roachcd@users.noreply.github.com>
2024-11-15 12:45:08 -0600

    Moved functions to vue methods

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <div id="map"></div>
     <div class="page">
         <div class="data">
-            <h1 id="countryName"></h1>
+            <h1 id="countryName">Global</h1>
             <h3>Most Similar</h3>
         </div>
     </div>
@@ -35,98 +35,13 @@ var countryMap = null;
 var info = L.control();
 
 info.onAdd = function (map) {
-    resetHighlight();
+    console.log("Created");
 };
-
-function dataExists(country){
-  const result = weightDataJson.filter(item => item.name === country);
-  if(result.length != 0){
-    return true;
-  }
-  return false;
-}
-
-info.update = function (props) {
-  if(dataExists(props)){
-    document.getElementById("countryName").textContent=props;
-  }
-  getWeightData(props);
-  countryMap.resetStyle();
-}
-
-function highlightFeature(e) {
-    var layer = e.target;
-    info.update(layer.feature.properties.name);
-
-    layer.setStyle({
-        weight: 5,
-        color: '#666',
-        dashArray: '',
-        fillOpacity: 0.7
-    });
-
-    layer.bringToFront();
-}
-
-function resetHighlight(e) {
-    info.update("Global");
-    //countryMap.resetStyle(e.target);
-} 
-
-function zoomToFeature(e) {
-    map.fitBounds(e.target.getBounds());
-}
-
-function onEachFeature(feature, layer) {
-    layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight,
-        click: zoomToFeature,
-    });
-}
-
-function getColor(country) {
-
-    const result = weightDataJson.filter(item => item.name === country);
-
-    //console.log(result);
-
-    if(result.length != 0){
-      const d = result[0].weight;
-
-      return d > 70 ? '#800026' :
-            d > 60  ? '#BD0026' :
-            d > 50  ? '#E31A1C' :
-            d > 40  ? '#FC4E2A' :
-            d > 30   ? '#FD8D3C' :
-            d > 20   ? '#FEB24C' :
-            d > 10   ? '#FED976' :
-                        '#FFEDA0';
-    }
-    else{
-      return '#d9d9d9';
-    }
-
-}
-
-function style(feature) {
-    return {
-        fillColor: getColor(feature.properties.name),
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7
-    };
-}
-
 
 export default {
   name: 'HomeView',
   components: {
   },
-  data(){
-    },
   methods:{
     setupLeafletMap: function () {
       const map = L.map('map', {zoomControl: false}).setView([0, 0], 2);
@@ -137,17 +52,92 @@ export default {
       }).addTo(map);
 
       var countryData = require("@/assets/globe.geo.json")
-      countryMap = new L.geoJson(countryData, {style: style, onEachFeature: this.hover});
+      countryMap = new L.geoJson(countryData, {style: this.style, onEachFeature: this.hover});
       countryMap.addTo(map);
 
-      info.addTo(map);      
+      info.addTo(map);    
+      this.infoOnAdd(map);  
     },
     hover(feature, layer){
-      onEachFeature(feature, layer)
+      this.onEachFeature(feature, layer)
+    },
+    style(feature) {
+      return {
+            fillColor: this.getColor(feature.properties.name),
+            weight: 2,
+            opacity: 1,
+            color: 'white',
+            dashArray: '3',
+            fillOpacity: 0.7
+        };
+    },
+    getColor(country) {
+      const result = weightDataJson.filter(item => item.name === country);
+
+      if(result.length != 0){
+        const d = result[0].weight;
+
+        return d > 70 ? '#800026' :
+              d > 60  ? '#BD0026' :
+              d > 50  ? '#E31A1C' :
+              d > 40  ? '#FC4E2A' :
+              d > 30   ? '#FD8D3C' :
+              d > 20   ? '#FEB24C' :
+              d > 10   ? '#FED976' :
+                          '#FFEDA0';
+      }
+      else{
+        return '#d9d9d9';
+      }
+    },
+    onEachFeature(feature, layer) {
+        layer.on({
+            mouseover: this.highlightFeature,
+            mouseout: this.resetHighlight,
+            click: this.zoomToFeature,
+        });
+    },
+    zoomToFeature(e) {
+        map.fitBounds(e.target.getBounds());
+    },
+    resetHighlight(e) {
+        this.updateInfo("Global");
+        //countryMap.resetStyle(e.target);
+    },
+    highlightFeature(e) {
+        var layer = e.target;
+        this.updateInfo(layer.feature.properties.name);
+
+        layer.setStyle({
+            weight: 5,
+            color: '#666',
+            dashArray: '',
+            fillOpacity: 0.7
+        });
+
+        layer.bringToFront();
+    },
+    dataExists(country){
+      const result = weightDataJson.filter(item => item.name === country);
+      if(result.length != 0){
+        return true;
+      }
+      return false;
+    },
+    updateInfo(props) {
+      if(this.dataExists(props)){
+        document.getElementById("countryName").textContent=props;
+      }
+      getWeightData(props);
+      countryMap.resetStyle();
+    },
+    infoOnAdd(map) {
+      resetHighlight();
     }
   },
   mounted() {
-   this.setupLeafletMap();
+    this.setupLeafletMap();
+    this.updateInfo("Global");
   },
 }
 </script>
